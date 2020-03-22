aac76eb917adf94df3bd02402b47497d33f8bcca
Bosco <nzibosco@gmail.com>
2020-03-22 19:06:01 -0400

    starting a spring boot application

---
+++ src/main/java/com/mthouse/AppDriver.java
@@ -0,0 +1,14 @@
+package com.mthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AppDriver {
+
+    public static void main (String ...args){
+        SpringApplication.run(AppDriver.class, args);
+    }
+}
---
+++ src/main/java/com/mthouse/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.mthouse.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test (){
+        return "Welcome to MT House API";
+    }
+}
