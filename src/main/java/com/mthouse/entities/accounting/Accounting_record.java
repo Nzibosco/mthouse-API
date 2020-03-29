8195a5e9b3deef3dd9f331549575679b843fbdbf
Bosco <nzibosco@gmail.com>
2020-03-28 22:30:17 -0400

    JPA annotations

--- src/main/java/com/mthouse/entities/accounting/Accounting_record.java
+++ src/main/java/com/mthouse/entities/accounting/Accounting_record.java
@@ -1,14 +1,38 @@
 package com.mthouse.entities.accounting;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
+@Entity @Data @Getter @Setter
 public class Accounting_record {
 
+    @Id @Column @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int recorder_id;
-    private Date record_date;
+
+    @Column(nullable = false)
+    private int recorderId;
+
+    @Column(nullable = false)
+    private LocalDateTime record_date;
+
+    @Column(nullable = false)
     private String business_line;
+
+    @Column(nullable = false)
     private String comment;
+
+    @Column(nullable = false)
     private double amount;
+
+    @Enumerated(EnumType.STRING)
     private Category category;
+
+    public void setDateRecorded(){
+        this.record_date = LocalDateTime.now();
+    }
 }
