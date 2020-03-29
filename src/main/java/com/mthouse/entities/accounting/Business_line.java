e9a376009f192479c4c4c0299786956a4143c356
Bosco <nzibosco@gmail.com>
2020-03-29 18:02:32 -0400

    JPA annotations

--- src/main/java/com/mthouse/entities/accounting/Business_line.java
+++ src/main/java/com/mthouse/entities/accounting/Business_line.java
@@ -1,12 +1,32 @@
 package com.mthouse.entities.accounting;
 
-import java.util.Date;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity @Data @Setter @Getter
 public class Business_line {
 
+    @Id @Column @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private int author_id;
-    private Date date_created;
+
+    @Column(nullable = false)
+    private LocalDateTime date_created;
+
+    @Column(nullable = false)
     private String line_name;
+
+    @Column(nullable = false)
     private String description;
+
+    public void setDate(){
+        this.date_created = LocalDateTime.now();
+    }
 }
